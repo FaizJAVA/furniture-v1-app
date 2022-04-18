421ed1443628801172e40b58a9a6be7694ad995d
jeetu-patil <jitupatil961@gmail.com>
2022-04-18 10:51:52 +0530

    jeetu-emi

--- controller/emicontrol.js
+++ controller/emicontrol.js
@@ -1,1 +1,111 @@
 const Emi=require('../model/emimodel');
+
+exports.emiEntry=(request,response)=>{
+    var totalWithInterest=(request.body.emiTotal*request.body.emiIntrest)/100;
+    totalWithInterest=request.body.emiTotal+totalWithInterest;
+
+    var installments=totalWithInterest/10;
+
+    var emiRemaining=totalWithInterest-(request.body.emiDownPayment);
+
+
+    Emi.create({
+        userId:request.body.userId,
+        products:request.body.products,
+        emiInstallment:installments,
+        emiMonth:10,
+        emiIntrest:request.body.emiIntrest,
+        emiTotal:request.body.emiTotal,
+        emiTotalWithIntresr:totalWithInterest,
+        emiDownPayment: request.body.emiDownPayment,
+        emiRemaining:emiRemaining
+    })
+    .then(result=>{
+        return response.status(200).json(result);
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+};
+
+//all emi for admin
+exports.adminAllEmi=(request,response)=>{
+    Emi.find({emiMonth:{$gt:0}})
+    .populate("userId")
+    .populate("products")
+    .then(result=>{
+        return response.status(200).json(result);
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+};
+
+//all emi history for addmin
+exports.adminHistoryEmi=(request,response)=>{
+    Emi.find({emiMonth:0})
+    .populate("userId")
+    .populate("products")
+    .then(result=>{
+        return response.status(200).json(result);
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+};
+
+
+//emi history for particular user 
+exports.userHistoryEmi=(request,response)=>{
+    Emi.find({emiMonth:0,userId:request.params.id})
+    .populate("userId")
+    .populate("products")
+    .then(result=>{
+        return response.status(200).json(result);
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+};
+
+//emi for user particular user
+exports.userEmi=(request,response)=>{
+    Emi.find({emiMonth:{$gt:0},userId:request.params.id})
+    .populate("userId")
+    .populate("products")
+    .then(result=>{
+        return response.status(200).json(result);
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+};
+
+exports.emiUpdate=(request,response)=>{
+
+    Emi.findOne({userId:request.params.id})
+    .then(result=>{
+        console.log(result);
+        console.log(result.emiMonth-1);
+        console.log(result.emiRemaining-result.emiInstallment);
+        Emi.updateOne({userId:request.params.id},
+            {
+                $set:
+                {
+                    emiMonth: result.emiMonth-1,
+                    emiRemaining:result.emiRemaining-result.emiInstallment
+                }
+            }    
+        )
+        .then(result=>{
+            return response.status(200).json(result);
+        })
+        .catch(err=>{
+            return response.status(500).json(err);
+        });
+    })
+    .catch(err=>{
+        return response.status(500).json(err);
+    });
+    
+};
