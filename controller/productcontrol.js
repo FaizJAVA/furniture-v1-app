c31c25a38abca943a9d4685374944527f2d52441
FaizJAVA <rockrad0007@gmail.com>
2022-04-22 16:51:19 +0530

    data

--- controller/productcontrol.js
+++ controller/productcontrol.js
@@ -1,7 +1,7 @@
 const productM=require('../model/productmodel');
 const {validationResult}=require('express-validator');
 const product=require('../model/productmodel');
-
+const path = require("path");
 const { Storage } = require('@google-cloud/storage');
 let bucketName = "gs://furniture-app-7e485.appspot.com"
 
@@ -35,12 +35,15 @@ exports.add=(request,response)=>{
         catId: request.body.catId
     })
     .then((result) => {
-        uploadFile(
-            path.join(__dirname,"../","public/images/")+request.file.filename
-        );
+        console.log(result);
+        
+            uploadFile(
+                path.join(__dirname,"../","public/images/")+request.file.filename
+            );
         return response.status(200).json(result);
     })
     .catch((err) => {
+        console.log(err);
         return response.status(500).json(err);
     });
 };
